e86ce4d974745f81bb7b5c9351c7dbc6c5e98b61
Balint Kovari <balintmaci@gmail.com>
2021-12-08 17:45:22 +0100

    implement code for getting variations

--- catalog.py
+++ catalog.py
@@ -19,12 +19,32 @@ def request_products():
     return requests.get(get_products_url_with_params(), headers=api.get_auth_header())
 
 
+def get_variations_url():
+    return "{}/{}".format(get_catalog_url(), "productsvariations.json")
+
+
+def get_variations_url_with_params(pageSize = 0, page = 0):
+    return "{}?pageSize={}&page={}".format(get_variations_url(), pageSize, page)
+
+
+def request_variations():
+    return requests.get(get_variations_url_with_params(), headers=api.get_auth_header())
+
+
+def get_variations_of_product_url(id):
+    return "{}/{}/{}.{}".format(get_catalog_url(), "productvariations", id, "json")
+
+
+def request_variations_of_product(id):
+    return requests.get(get_variations_of_product_url(id), headers=api.get_auth_header())
+
+
 def get_products():
-    req1 = request_products()
-    if req1.status_code == 200:
-        return json.load(req1.text)
+    r = request_products()
+    if r.status_code == 200:
+        return json.loads(r.text)
     else:
-        print(req1.text)
+        print(r.text)
         return None
 
 
@@ -33,5 +53,19 @@ def get_dummy_products():
         return json.load(f)
 
 
+def get_variations():
+    r = request_variations()
+    if r.status_code == 200:
+        return json.loads(r.text)
+    else:
+        print(r.text)
+        return None
+
+
 if __name__ == "__main__":
-    print(get_dummy_products()[0])
+    dummy_products = get_dummy_products()
+    print(dummy_products[1])
+    all_vars = get_variations()
+    with open("variations.txt", "w") as f:
+        json.dump(all_vars,f)
+
