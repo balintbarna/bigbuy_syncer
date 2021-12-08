1156aa948517d23f87032d0d309851ffdd15ddb8
Balint Kovari <balintmaci@gmail.com>
2021-12-08 21:57:31 +0100

    get statistics about bigbuy products

--- bigbuy/catalog/exports.py
+++ bigbuy/catalog/exports.py
@@ -25,5 +25,31 @@ def find_matching_product_and_variant():
                 return
 
 
+def compare_product_and_variant_numbers():
+    prods = get_products(dummy=True)
+    vars = get_variations(dummy=True)
+    print("number of products: {}".format(len(prods)))
+    print("number of variants: {}".format(len(vars)))
+    orphan_variant_ids = []
+    unique_product_ids = []
+    for variant in vars:
+        product_id = variant["product"]
+        if product_id not in unique_product_ids:
+            unique_product_ids.append(product_id)
+        if not find_product_for_variant(variant, prods):
+            orphan_variant_ids.append(variant["id"])
+    print("number of unique products in variants list: {}".format(len(unique_product_ids)))
+    print("number of orphan variants: {}".format(len(orphan_variant_ids)))
+
+
+def find_product_for_variant(variant, product_list):
+    product_id = variant["product"]
+    for product in product_list:
+        if product["id"] == product_id:
+            return product
+
+
+
 if __name__ == "__main__":
     find_matching_product_and_variant()
+    compare_product_and_variant_numbers()
