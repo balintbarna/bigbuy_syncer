54c28717cbcfa5ce8fd5608a39f51acf6ad4dc1a
Balint Kovari <balintmaci@gmail.com>
2021-12-08 17:25:10 +0100

    add hint about api key

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 def read_api_key():
-    with open("api_key") as f:
-        return f.readline()
+    try:
+        with open("api_key") as f:
+            return f.readline()
+    except:
+        print("Must have top-level file called \"api_key\" which contains only the secret key in one line")
 
 
 api_key = read_api_key()
